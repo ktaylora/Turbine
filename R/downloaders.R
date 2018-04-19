2e240416e28540482700c2c37d6184a607b27d5e
Kyle Taylor <kyle.a.taylor@gmail.com>
2018-04-19 11:40:55 -0600

    added an interface for generating a SpatialPointsDataFrame from a fetched FAA obstruction zip. Validated working.

--- R/downloaders.R
+++ R/downloaders.R
@@ -34,3 +34,64 @@ web_scrape_faa_digital_obstructions <- function(FAA_OBSTRUCTIONS="https://www.fa
   # return the file path to the user
   return(file)
 }
+# ' hidden function that will unpack an FAA zipfile and return as SpatialPointsDataFrame to the user.
+# '
+# @param x zipfile name containing FAA obstruction data
+# @param write optionally write SpatialPointsDataFrame to current working directory.
+# @value returns wind turbine data to the user as a SpatialPointsDataFrame
+unpack_faa_zip <- function(x,write=T){
+  unzip(x)
+
+  t <- read.csv("DOF.DAT",t=" ",header=F,skip=4,stringsAsFactors=F)
+  t <- t[apply(t,1,function(x) sum(grepl(x,pattern="WINDMILL"))) > 0,] # remove everything that doesn't a WINDMILL field somewhere
+
+  cat(" -- processing ", length(t), " turbine points:", sep="")
+
+  coords <- do.call(rbind, lapply(
+      X=1:length(t),
+      FUN=function(i){
+        ln <- strsplit(t[i],split=" ")[[1]]
+        # parse northing and westing, then convert to decimal degrees (WGS84)
+        # NORTHING
+        northing <- ln[nchar(ln)==6]
+          northing <- northing[grepl(northing[nchar(northing)==6],pattern=".*.[.].*N")]
+        degrees_n <- as.numeric(ln[which(grepl(ln,pattern=northing))-2])
+          minutes_n <- as.numeric(ln[which(grepl(ln,pattern=northing))-1])
+            seconds_n <- as.numeric(substr(northing,1,nchar(northing)-1))
+        northing <- degrees_n+(minutes_n/60)+(seconds_n/3600)
+        # WESTING
+        westing <- ln[nchar(ln)==6]
+        if(sum(grepl(westing[nchar(westing)==6],pattern=".*.[.].*W"))>0){ # are we using a true westing coordinate?
+          westing <- westing[grepl(westing[nchar(westing)==6],pattern=".*.[.].*W")]
+          degrees_w <- as.numeric(ln[which(grepl(ln,pattern=westing))-2])
+            minutes_w <- as.numeric(ln[which(grepl(ln,pattern=westing))-1])
+              seconds_w <- as.numeric(substr(westing,1,nchar(westing)-1))
+          westing <- -1*(degrees_w+(minutes_w/60)+(seconds_w/3600))
+        } else if(sum(grepl(westing[nchar(westing)==6],pattern=".*.[.].*E"))>0){ # this must be an easting coordinate
+          westing <- westing[grepl(westing[nchar(westing)==6],pattern=".*.[.].*E")]
+          degrees_w <- as.numeric(ln[which(grepl(ln,pattern=westing))-2])
+            minutes_w <- as.numeric(ln[which(grepl(ln,pattern=westing))-1])
+              seconds_w <- as.numeric(substr(westing,1,nchar(westing)-1))
+          westing <- degrees_w+(minutes_w/60)+(seconds_w/3600)
+        } else {
+          stop(paste("unknown westing coordinate at:",westing))
+        }
+        # YEAR
+        year <- as.numeric(substr(ln[length(ln)-1],1,4))
+        # return
+        if(i%%10==0){ cat(".") }
+        return(data.frame(x=westing,y=northing,year=year))
+        #coords <- rbind(coords,data.frame(x=westing,y=northing,year=year))
+        # if(i%%10==0){ cat(".") }
+      }
+  )); cat("\n");
+  # re-format as a SpatialPointsDataFrame and return to user
+  pts <- sp::SpatialPointsDataFrame(coords=data.frame(x=coords$x,y=coords$y),data=data.frame(year=coords$year))
+    raster::projection(pts) <- raster::projection("+init=epsg:4326")
+  if(write){
+    rgdal::writeOGR(pts,".",paste(unlist(strsplit(x,split="[.]"))[[1]],"_pts",sep=""),driver="ESRI Shapefile",overwrite=T)
+  }
+  # clean-up
+  unlink(list.files(pattern=".DAT$|.Dat$"))
+  return(pts)
+}

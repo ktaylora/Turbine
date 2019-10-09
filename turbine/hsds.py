b42c7614ed0c2f2411c60104516010dd0faf0d34
Kyle Taylor <kyle.a.taylor@gmail.com>
2019-10-09 08:26:09 -0600

    house keeping

--- turbine/hsds.py
+++ turbine/hsds.py
@@ -16,12 +16,13 @@ def extent_to_hsds_bounding_box(extent=None):
     sw = (2,2)
     return (ne, sw)
 
-def rasterize(shapefile_path=None, template=None, outfile=None, **kwargs):
+def rasterize(shapefile_path=None, template=None, outfile=None, burn_values=1, **kwargs):
     """
-    Quick and hackish approach to rasterizing a vector dataset stored as a shapefile on disk 
+    Quick and hackish approach to rasterizing a vector dataset stored as a shapefile on disk
     using gdal.RasterizeLayer. I haven't found a faster way to do this.
     """
     kwargs['dst_filename'] = kwargs.get('dst_filename', outfile)
+
     kwargs['eType'] = kwargs.get('eType', gdal.GDT_Int32)
     kwargs['xsize'] = kwargs.get('xsize', abs(template.geot[1]))
     kwargs['ysize'] = kwargs.get('ysize', abs(template.geot[5]))
@@ -30,9 +31,14 @@ def rasterize(shapefile_path=None, template=None, outfile=None, **kwargs):
     target_ds = gdal.GetDriverByName('GTiff').Create(**kwargs)
     layer = ogr.Open(shapefile_path).get_layer()
 
-    gdal.RasterizeLayer(dataset=target_ds, bands=kwargs['bands'], layer=layer, burn_values=1)
+    gdal.RasterizeLayer(
+        dataset=target_ds, 
+        bands=kwargs['bands'], 
+        layer=layer, 
+        burn_values=burn_values
+    )
+    
     target_ds.FlushCache()
-    del target_ds
 
 def h5_to_geodataframe(dataset_name=None):
     f = h5.File("/nrel/wtk-us.h5", 'r')
@@ -40,7 +46,7 @@ def h5_to_geodataframe(dataset_name=None):
     #x,y = zip(*f['coordinates'].flatten())
     coords = f['coordinates'][:].flatten()
     _gdf = GeoDataFrame({
-        'geometry' : GeoSeries([Point(i) for i in coords])
+        'geometry' : GeoSeries([Point(reversed(i)) for i in coords])
     })
     # merge in our attributes
     _gdf = _gdf.join(ds)
